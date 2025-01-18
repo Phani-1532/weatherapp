68aa7ea520441c9383bff400ef8564ee9623e9c8
Phanindra <phanindragadireddy2@gmail.com>
2025-01-18 09:35:13 +0530

    updated versions

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'weatherapp',
+  location: 'us-central1'
+};
+
